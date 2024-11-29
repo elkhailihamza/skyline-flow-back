cdb674c8553e596a3f15de0ebfb4802cb658dcac
elkhailihamza <elkhailihamza331@gmail.com>
2024-11-29 11:22:57 +0100

    added content meta data repository

---
+++ src/main/java/org/project/skyflow/repository/ContentMetaDataRepository.java
@@ -0,0 +1,7 @@
+package org.project.skyflow.repository;
+
+import org.project.skyflow.entity.ContentMetaData;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ContentMetaDataRepository extends JpaRepository<ContentMetaData, Long> {
+}
