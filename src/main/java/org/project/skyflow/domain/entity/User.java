f3bb5b8f7de8f7a95edfe6a5d180083946ac8db2
elkhailihamza <elkhailihamza331@gmail.com>
2025-01-08 09:36:22 +0100

    made roles for user eagerly fetched

--- src/main/java/org/project/skyflow/domain/entity/User.java
+++ src/main/java/org/project/skyflow/domain/entity/User.java
@@ -34,7 +34,7 @@ public class User {
     private boolean isSuspended;
     private boolean isActive;
 
-    @ManyToMany
+    @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(
             name = "user_roles",
             joinColumns = @JoinColumn(name = "user_id"),
