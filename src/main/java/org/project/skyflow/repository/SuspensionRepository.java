366770167b44a58a8a6fb76f5595d121f04d180b
elkhailihamza <elkhailihamza331@gmail.com>
2025-01-08 09:35:43 +0100

    added repositories for role and suspension tables

---
+++ src/main/java/org/project/skyflow/repository/SuspensionRepository.java
@@ -0,0 +1,7 @@
+package org.project.skyflow.repository;
+
+import org.project.skyflow.domain.entity.Suspension;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SuspensionRepository extends JpaRepository<Suspension, Long> {
+}
